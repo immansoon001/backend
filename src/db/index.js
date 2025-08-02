70657d13be227d3f0722be250d17f6ee99b16a51
immansoon001 <mansoonp001@gmail.com>
2025-08-03 01:33:42 +0530

    connect to mongoose

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose, { connect } from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+const connectDB = async () => {
+    try {
+    const connectionInstances=   await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    
+    console.log(`\n MongoDB connected !! DB HOST :${connectionInstances.connection.host}`);
+    
+    } catch(error){
+console.log("MONGODB connections error",error);
+process.exit(1)
+
+    }
+
+}
+export default connectDB
